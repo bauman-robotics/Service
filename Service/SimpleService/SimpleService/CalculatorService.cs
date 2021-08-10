34c3a2f676abfb66f05dadd7ea034d8ab8132ae5
Bauman Robotics | BMSTU <baumanrobotics@gmail.com>
2021-08-10 22:46:50 +0300

    Создали простой сервис

---
+++ Service/SimpleService/SimpleService/CalculatorService.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleService
+{
+    public class CalculatorService : ICalculatorService
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Service/SimpleService/SimpleService/ICalculatorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleService
+{
+    public interface ICalculatorService
+    {
+        double Add(double a, double b);
+
+        double Sub(double a, double b);
+    }
+}
